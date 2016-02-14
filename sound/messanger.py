dacc11365b39ee45bfea727e7f5b6787a62823a1
Eran Egozy <eran@egozy.org>
2016-02-14 13:40:21 -0500

    made heartbeat work (no heartbeat = disconnected)

--- sound/messanger.py
+++ sound/messanger.py
@@ -53,8 +53,7 @@ class Messanger(threading.Thread):
       self.client.connect( (send_ip, send_port) )
 
       # member vars
-      # self.last_heartbeat_time = 0
-      self.server_connected = False
+      self.heartbeat_time = 0
       self.server_retry = 0
       self.done_running = False
 
@@ -86,7 +85,7 @@ class Messanger(threading.Thread):
 
    def on_update(self):
       now = time.time()
-      if not self.server_connected and self.server_retry < now:
+      if not self.is_connected() and self.server_retry < now:
          # try to connect to condcutor
          try:
             msg = OSCMessage('/max', ('hello', self.listen_ip, self.listen_port))
@@ -104,26 +103,13 @@ class Messanger(threading.Thread):
       osc_msg = OSCMessage('/max', ('note', player_idx, note))
       self.client.send(osc_msg)
 
-
    def is_connected(self):
-      return self.server_connected
-
-      # now = time.time()
-      # send_heartbeat = now - self.last_heartbeat_time > 3.0
-      # if send_heartbeat:
-      #    self.last_heartbeat_time = now
-
-      # try:
-      #    for j in self.active_joints:
-      #       if send_heartbeat:
-      #          #print 'heartbeat:', j
-      #          self.client.send( OSCMessage(j + "_trackjointpos", 1) )
-      # except Exception as x:
-      #    print x, 'sending to', self.client.client_address
+      now = time.time()      
+      return self.heartbeat_time + 2.0 > now
 
    def cb_heartbeat(self, path, tags, args, source):
       # print 'got ', path, tags, args
-      self.server_connected = True 
+      self.heartbeat_time = time.time()
 
    def cb_section_idx(self, path, tags, args, source):
       self.queue.put((path, args))
